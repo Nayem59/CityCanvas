2744b489321eef5dc373a4c5c3bcfa6b000cec13
Nayem <nayem.alam97@gmail.com>
2023-03-27 16:05:59 +0100

    removed test Home

--- CityCanvas/App.js
+++ CityCanvas/App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import Home from "./components/Home";
 
 export default function App() {
   return (
     <View>
-      <Home />
       <Text className="text-red-500">
         Open up App.js to start working on your app!
       </Text>
