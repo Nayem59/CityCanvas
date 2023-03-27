c9d4e896d65ccf7e24ffbc9c7c99778f3fc49646
mibrahim97 <106470649+mibrahim97@users.noreply.github.com>
2023-03-27 16:57:00 +0100

    basic tailwind config

--- CityCanvas/tailwind.config.js
+++ CityCanvas/tailwind.config.js
@@ -6,7 +6,40 @@ module.exports = {
     "./screens/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      // main colour
+      pink: "#C13584",
+      // input field & nav icons
+      "light-gray": "#BDBABA",
+      //headings
+      "md-gray": "#9B9B9B",
+      //text colour
+      black: "#000000",
+      //text colour
+      white: "#FFFFFF",
+      //pins on map
+      blue: "#0476D0",
+    },
+    //all fonts are the same
+    fontFamily: {
+      // fontSize:24px
+      poppins: ["Poppins", "sans-serif"],
+      sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {
+      borderRadius: {
+        "5xl": "2rem",
+        "1xl": "2rem",
+      },
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+      },
+    },
   },
   plugins: [],
 };
