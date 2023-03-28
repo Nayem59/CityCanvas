08366a68afe079e763ef76e9e41b51b182831e10
FameTii <shinfameti@gmail.com>
2023-03-28 13:38:18 +0100

    can consolelog list

--- CityCanvas/screens/ArtList.js
+++ CityCanvas/screens/ArtList.js
@@ -1,12 +1,58 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+// import { View, Text, SafeAreaView } from "react-native";
+// import React from "react";
+
+// const ArtList = () => {
+// 	return (
+// 		<SafeAreaView>
+// 			<Text>ArtList</Text>
+// 		</SafeAreaView>
+// 	);
+// };
+
+// export default ArtList;
+
+import { View, Text, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebaseConfig";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { collection, getDocs, setDoc, onSnapshot } from "firebase/firestore";
 
 const ArtList = () => {
-	return (
-		<SafeAreaView>
-			<Text>ArtList</Text>
-		</SafeAreaView>
-	);
+  const [isLoading, setIsLoading] = useState(true);
+  const artCol = collection(db, "art");
+  const [art, setArt] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    function getArtWork() {
+      return getDocs(artCol)
+        .then((artworksSnapShot) => {
+          const artWorkList = artworksSnapShot.docs.map((doc) => doc.data());
+          setArt(artWorkList);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+    getArtWork();
+  }, []);
+  console.log(art);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          <FlatList
+            data={artCol}
+            renderItem={({ item }) => {
+              return <Text>{item.name}</Text>;
+            }}
+          />
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default ArtList;
