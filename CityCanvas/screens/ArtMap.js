623ed1737e92e0396adf1f4c8895185158aecb27
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-28 10:24:11 +0100

    navgiation complete

--- CityCanvas/screens/ArtMap.js
+++ CityCanvas/screens/ArtMap.js
@@ -1,8 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, Touchable, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native";
 
-const ArtMap = () => {
+const ArtMap = ({ navigation }) => {
 	return (
 		<SafeAreaView>
 			<Text>ArtMap</Text>
