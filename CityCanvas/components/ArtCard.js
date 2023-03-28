ef802deb737f87e794d8985a56d6937a934e25f6
Anna Chernova <annanvks@gmail.com>
2023-03-28 16:59:45 +0100

    additems rendered on a screen

--- CityCanvas/components/ArtCard.js
+++ CityCanvas/components/ArtCard.js
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
 
-const ArtCard = ({item}) => {
+const ArtCard = ({ item }) => {
   console.log(item);
   return (
     <View>
       <Text>{item.title}</Text>
-    </View>
-  )
-}
 
-export default ArtCard
+      <Text>{item.artist}</Text>
+      <Image
+        style={{
+          height: 150,
+          width: 150,
+          borderRadius: 20,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        src={item.image}
+      />
+      <Text>
+        {item.address_building_number} {item.address_street},{" "}
+        {item.address_city} {item.address_postcode}
+      </Text>
+      <Text>{item.likes_count}</Text>
+    </View>
+  );
+};
+
+export default ArtCard;
