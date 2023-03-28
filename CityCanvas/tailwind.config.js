ff940c3a1dcea6fc210235c0382f4d25f775e8ad
mibrahim97 <106470649+mibrahim18@users.noreply.github.com>
2023-03-28 10:15:05 +0100

    config all finished apart from poppins font

--- CityCanvas/tailwind.config.js
+++ CityCanvas/tailwind.config.js
@@ -6,34 +6,40 @@ module.exports = {
     "./screens/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      // main colour
-      pink: "#C13584",
-      // input field & nav icons
-      "light-gray": "#BDBABA",
-      //headings
-      "md-gray": "#9B9B9B",
-      //text colour
-      black: "#000000",
-      //text colour
-      white: "#FFFFFF",
-      //pins on map
-      blue: "#0476D0",
-    },
-    //all fonts are the same
-    fontFamily: {
-      // fontSize:24px
-      poppins: ["Poppins", "sans-serif"],
-      sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
-    },
     extend: {
+      colors: {
+        // main colour
+        pink: "#C13584",
+        // input field & nav icons
+        "light-gray": "#BDBABA",
+        //headings
+        "md-gray": "#9B9B9B",
+        //text colour
+        black: "#000000",
+        //text colour
+        white: "#FFFFFF",
+        //pins on map
+        blue: "#0476D0",
+      },
+
+      fontFamily: {
+        //all fonts are the same
+        // fontSize:24px - headers
+        // fontSize:15px - input/text
+        // fontSize:10px - link text for location and file upload
+        // fontSize:13px - button text/not yet registered text
+        // fontSize:16px - continue with apple/google text
+        //poppins doesn't work
+        poppins: ["Poppins", "sans-serif"],
+        sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+      },
       borderRadius: {
-        "5xl": "2rem",
-        "1xl": "2rem",
+        //pics/text cards - 20px
+        brm: "1.25rem",
+        //buttons 4px
+        brs: "0.25rem",
       },
       spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
         sm: "8px",
         md: "12px",
         lg: "16px",
