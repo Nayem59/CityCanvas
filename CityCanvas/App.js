2594dabda757b69aa6846fefb52840e6ac34d3eb
Claire Lancaster <hello@clairelancaster.com>
2023-03-28 09:58:04 +0100

    firebase config added  and firestore integrated

--- CityCanvas/App.js
+++ CityCanvas/App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 export default function App() {
+  
   return (
     <View>
       <Text className="text-red-500">
---
+++ CityCanvas/city-canvas/city-canvas/city_canvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  city_canvasApp.swift
+//  city-canvas
+//
+//  Created by Claire Lancaster on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct city_canvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
