d6ab3b520bffff3922ddd96771e7e5167f75e85d
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-29 21:17:31 +0100

    completed sign in page and created reusable components

--- CityCanvas/components/Input.js
+++ CityCanvas/components/Input.js
@@ -1,12 +1,27 @@
-import { View, Text } from "react-native";
+import { View, TextInput } from "react-native";
 import React from "react";
 
-const Input = () => {
-  return (
-    <View>
-      <Text>Input</Text>
-    </View>
-  );
+const Input = ({
+	placeholder,
+	value,
+	setEmail,
+	autoCapitalize,
+	secure,
+	autoCorrect,
+}) => {
+	return (
+		<View className="w-full">
+			<TextInput
+				placeholder={placeholder}
+				value={value}
+				onChangeText={(input) => setEmail(input)}
+				autoCapitalize={autoCapitalize}
+				secureTextEntry={secure}
+				autoCorrect={autoCorrect}
+				className="w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink"
+			/>
+		</View>
+	);
 };
 
 export default Input;
