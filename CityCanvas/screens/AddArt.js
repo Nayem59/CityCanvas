f3bb0129b95bbe61a9fdaf1018f0e223692cd1b3
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-30 16:42:50 +0100

    completed most functionality will need to add formik today

--- CityCanvas/screens/AddArt.js
+++ CityCanvas/screens/AddArt.js
@@ -1,22 +1,59 @@
-import { View, Text, SafeAreaView, TextInput, Button } from "react-native";
+import {
+	View,
+	Text,
+	SafeAreaView,
+	TextInput,
+	TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
-import Input from "../components/Input";
-import AppButton from "../components/AppButton";
 import { MultipleSelectList } from "react-native-dropdown-select-list";
-import { launchCamera, launchImageLibrary } from "react-native-image-picker";
 import { FontAwesome } from "@expo/vector-icons";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { AntDesign } from "@expo/vector-icons";
+import * as Location from "expo-location";
+import * as ImagePicker from "expo-image-picker";
+
 const AddArt = () => {
 	const [selectedTags, setSelectedTags] = useState([]);
+	const [userLocation, setUserLocation] = useState("");
+	const [image, setImage] = useState(null);
+
+	const pickImage = async () => {
+		// No permissions request is necessary for launching the image library
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setImage(result.assets[0].uri);
+		}
+	};
+
+	const getPermissionsAndLocation = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			Alert.alert("Please grant location permissions");
+			return;
+		}
+		const currentLocation = await Location.getCurrentPositionAsync({});
+		const reversedLocation = await Location.reverseGeocodeAsync({
+			longitude: currentLocation.coords.longitude,
+			latitude: currentLocation.coords.latitude,
+		});
+		const { city, postalCode } = reversedLocation[0];
+		const location = `${city}, ${postalCode}`;
+		setUserLocation(location);
+	};
+
 	const [date, setDate] = useState(new Date());
-	const [showPicker, setShowPicker] = useState(false);
 
 	const onChange = ({ type }, selectedDate) => {
 		if (type == "set") {
 			const currentDate = selectedDate;
 			setDate(currentDate);
-		} else {
-			togglePicker();
 		}
 	};
 
@@ -31,34 +68,77 @@ const AddArt = () => {
 		{ key: "8", value: "Reverse Graffiti" },
 	];
 
-	const togglePicker = () => {
-		setShowPicker(!showPicker);
-	};
-
 	return (
-		<SafeAreaView className="items-center justify-center w-full h-full bg-white">
-			<Text className="mt-10 mb-10 text-3xl text-center">Post Art</Text>
+		<SafeAreaView className="items-center w-full h-full bg-white">
+			<Text className="mt-10 mb-10 text-3xl text-center">
+				Upload <Text className="font-bold text-pink">Street Art</Text>
+			</Text>
 			<View className="w-10/12">
 				<View className="w-full">
+					<View className="flex flex-row items-center justify-between w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink">
+						<TextInput
+							placeholder={"Upload image"}
+							autoCapitalize="none"
+							value={image}
+							onChange={(input) => setImage(input)}
+						/>
+						<TouchableOpacity className="px-2 border-l border-stone-300">
+							<FontAwesome
+								name="upload"
+								size={20}
+								color={"#c13584"}
+								style={{ marginLeft: 10 }}
+								onPress={pickImage}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+				<View className="w-full">
 					<TextInput
-						placeholder={"Image"}
+						placeholder="Title"
 						autoCapitalize="none"
 						className="w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink"
 					/>
 				</View>
-
-				<Input placeholder={"Art Name"} />
-				<View className="flex flex-row w-full p-3 py-2 my-2 border rounded-lg border-stone-300 focus:border-pink">
-					{showPicker && (
-						<DateTimePicker
-							mode="date"
-							value={date}
-							textColor="black"
-							// display="spinner"
-							onChange={onChange}
-							placeholderTextColor="#d6d3d1"
+				<View className="w-full">
+					<TextInput
+						placeholder="Description"
+						autoCapitalize="none"
+						className="w-full p-3 pb-32 my-2 border rounded-lg border-stone-300 focus:border-pink"
+					/>
+				</View>
+				<View className="w-full ">
+					<View className="flex flex-row items-center justify-between w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink">
+						<TextInput
+							placeholder="Location"
+							autoCapitalize="none"
+							value={userLocation}
+							onChange={(input) => setUserLocation(input)}
 						/>
-					)}
+						<TouchableOpacity
+							className="px-2 border-l border-stone-300"
+							onPress={getPermissionsAndLocation}
+						>
+							<FontAwesome
+								name="location-arrow"
+								size={20}
+								color={"#c13584"}
+								style={{ marginLeft: 10 }}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+
+				<View className="flex flex-row justify-between w-full p-3 py-2 my-2 border rounded-lg border-stone-300 focus:border-pink">
+					<Text className="mt-2 text-stone-300">Date</Text>
+					<DateTimePicker
+						mode="date"
+						value={date}
+						textColor="black"
+						accentColor={"#c13584"}
+						onChange={onChange}
+						placeholderTextColor="#d6d3d1"
+					/>
 				</View>
 				<View className="mt-2">
 					<MultipleSelectList
@@ -74,12 +154,19 @@ const AddArt = () => {
 						searchicon={
 							<FontAwesome name="search" size={20} color={"#c13584"} />
 						}
-						maxHeight={200}
+						maxHeight={150}
 						boxStyles={{ borderColor: "#d6d3d1" }}
 						labelStyles={{ color: "#d6d3d1" }}
 						defaultOption={"Graffiti"}
 					/>
-					<AppButton primary={true} title={"post"} icon={"mail"} />
+				</View>
+				<View className="flex items-end justify-end h-32">
+					<TouchableOpacity className="flex flex-row items-center justify-center w-full p-3 py-4 my-2 rounded-lg bg-pink border-stone-300">
+						<AntDesign name={"mail"} size={20} color="white" />
+						<Text className="ml-4 font-bold text-white uppercase text-md">
+							POST
+						</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 		</SafeAreaView>
