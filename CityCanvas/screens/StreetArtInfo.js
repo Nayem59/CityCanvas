4f7fe596d24364ab7aced2fe908292efeee069b2
mibrahim97 <106470649+mibrahim18@users.noreply.github.com>
2023-03-30 11:49:58 +0100

    singleArt styling done

--- CityCanvas/screens/StreetArtInfo.js
+++ CityCanvas/screens/StreetArtInfo.js
@@ -1,11 +1,27 @@
-import { View, Text, Image, Button, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
 import { getDoc, doc } from "firebase/firestore";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import AppButton from "../components/AppButton";
 
-const StreetArtInfo = ({ route, navigation }) => {
+const StreetArtInfo = ({
+  route,
+  navigation,
+  title,
+  primary,
+  handlePress,
+  icon,
+}) => {
   const [artInfo, setArtInfo] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const { id } = route.params;
@@ -30,22 +46,51 @@ const StreetArtInfo = ({ route, navigation }) => {
         <Text>Loading...</Text>
       ) : (
         <View>
-          <Image className="h-32 w-32 rounded-3xl m-2" src={artInfo.image} />
-          <Text>{artInfo.title}</Text>
-          <Text>{artInfo.artist}</Text>
-          <Text>
-            {artInfo.address_building_number} {artInfo.address_street},{" "}
-            {artInfo.address_city} {artInfo.address_postcode}
-          </Text>
-          <Text>{artInfo.likes_count}</Text>
-          <Text>{artInfo.date_created.toDate().toDateString()}</Text>
-
-          <TouchableHighlight onPress={() => {}}>
-            <View>
-              <Button title="Get Location"></Button>
-              <Entypo name="location-pin" size={24} color="black" />
+          <View className="flex flex-column items-center gap-1 p-1">
+            <Image
+              className="h-64 w-64 flex items-stretch rounded-3xl m-2 border-2"
+              src={artInfo.image}
+            />
+          </View>
+          <View className="mx-2 gap-1 p-2 border-2 border-light-gray rounded-3xl">
+            <View className="flex flex-column items-center">
+              <Text className="-mb-2 text-lg font-bold color-pink">
+                {artInfo.title ? artInfo.title : "Untitled"}
+              </Text>
+              <Text className="mt-1 text-xs">
+                By {artInfo.artist ? artInfo.artist : "Unknown"}
+              </Text>
+              <Text className="font-semibold color-md-gray brightness-50 text-xs mb-2">
+                {artInfo.date_created.toDate().toDateString()}
+              </Text>
             </View>
-          </TouchableHighlight>
+            <Text className="mb-2 text-justify">{artInfo.description}</Text>
+            {/* <Text className="mb-2">{artInfo.likes_count}</Text> */}
+            <Text className="mb-2">
+              <Entypo name="location-pin" size={20} color="#C13584" />
+              {artInfo.address_building_number} {artInfo.address_street},{" "}
+              {artInfo.address_city} {artInfo.address_postcode}
+            </Text>
+          </View>
+          {/* absolute -bottom-60 left-0 */}
+
+          <View className="mt-24 mb-4 text-stone-500">
+            <AppButton
+              title={"Comments"}
+              handlePress={() => {
+                console.log("Comments Clicked");
+              }}
+              icon={"message1"}
+            />
+            <AppButton
+              title={"Get Route"}
+              handlePress={() => {
+                console.log("Get Route Clicked");
+              }}
+              primary={true}
+              icon={"user"}
+            />
+          </View>
         </View>
       )}
     </SafeAreaView>
