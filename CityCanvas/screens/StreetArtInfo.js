fa979995350155dd15ff8cd7a60ad00df17e887e
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-30 21:50:59 +0100

    made changes to the street info file

--- CityCanvas/screens/StreetArtInfo.js
+++ CityCanvas/screens/StreetArtInfo.js
@@ -1,13 +1,13 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
 import { getDoc, doc } from "firebase/firestore";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, ActivityIndicator } from "react-native";
 import { Entypo } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import AppButton from "../components/AppButton";
+import { Ionicons } from "@expo/vector-icons";
 
-const StreetArtInfo = ({ route }) => {
+const StreetArtInfo = ({ route, navigation }) => {
 	const [artInfo, setArtInfo] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
 	const { id } = route.params;
@@ -25,55 +25,70 @@ const StreetArtInfo = ({ route }) => {
 	}, [id]);
 
 	return (
-		<SafeAreaView>
+		<SafeAreaView className="absolute w-full h-full bg-white">
 			{isLoading ? (
-				<Text>Loading...</Text>
+				<View className="flex items-center justify-center flex-1">
+					<ActivityIndicator size="large" color="#C13584" />
+					<Text className="text-pink">loading</Text>
+				</View>
 			) : (
 				<View>
-					<View className="flex items-center gap-1 p-1 flex-column">
+					<View className="flex items-center py-4 mb-2 h-max">
+						<TouchableOpacity
+							className="relative z-10 right-44"
+							onPress={() => navigation.goBack()}
+						>
+							<Ionicons
+								name="chevron-back-circle-outline"
+								size={38}
+								color="#C13584"
+							/>
+						</TouchableOpacity>
 						<Image
-							className="flex items-stretch w-64 h-64 m-2 border-2 rounded-3xl"
+							className="flex items-center w-full h-72"
 							src={artInfo.image}
 						/>
 					</View>
-					<View className="gap-1 p-2 mx-2 border-2 border-light-gray rounded-3xl">
-						<View className="flex items-center flex-column">
-							<Text className="-mb-2 text-lg font-bold color-pink">
-								{artInfo.title ? artInfo.title : "Untitled"}
+					<View className="flex w-10/12 mx-auto">
+						<View className=" rounded-3xl">
+							<View className="flex items-center flex-column">
+								<Text className="-mb-2 text-lg font-bold color-pink">
+									{artInfo.title ? artInfo.title : "Untitled"}
+								</Text>
+								<Text className="mt-1">
+									By {artInfo.artist ? artInfo.artist : "Unknown"}
+								</Text>
+								<Text className="mb-2 font-semibold text-stone-400">
+									{artInfo.date_created.toDate().toDateString()}
+								</Text>
+							</View>
+							<Text className="mb-2 text-lg text-justify h-44">
+								{artInfo.description}
 							</Text>
-							<Text className="mt-1 text-xs">
-								By {artInfo.artist ? artInfo.artist : "Unknown"}
-							</Text>
-							<Text className="mb-2 text-xs font-semibold color-md-gray brightness-50">
-								{artInfo.date_created.toDate().toDateString()}
+							<Text className="mb-1 text-stone-400">
+								<Entypo name="location-pin" size={20} color="#C13584" />
+								{artInfo.address_building_number} {artInfo.address_street},{" "}
+								{artInfo.address_city} {artInfo.address_postcode}
 							</Text>
 						</View>
-						<Text className="mb-2 text-justify">{artInfo.description}</Text>
-						{/* <Text className="mb-2">{artInfo.likes_count}</Text> */}
-						<Text className="mb-2">
-							<Entypo name="location-pin" size={20} color="#C13584" />
-							{artInfo.address_building_number} {artInfo.address_street},{" "}
-							{artInfo.address_city} {artInfo.address_postcode}
-						</Text>
-					</View>
-					{/* absolute -bottom-60 left-0 */}
 
-					<View className="mt-24 mb-4 text-stone-500">
-						<AppButton
-							title={"Comments"}
-							handlePress={() => {
-								console.log("Comments Clicked");
-							}}
-							icon={"message1"}
-						/>
-						<AppButton
-							title={"Get Route"}
-							handlePress={() => {
-								console.log("Get Route Clicked");
-							}}
-							primary={true}
-							icon={"user"}
-						/>
+						<View className="mt-3 text-stone-500">
+							<AppButton
+								title={"Comments"}
+								handlePress={() => {
+									console.log("Comments Clicked");
+								}}
+								icon={"message1"}
+							/>
+							<AppButton
+								title={"Get Direction"}
+								handlePress={() => {
+									console.log("Get Route Clicked");
+								}}
+								primary={true}
+								icon={"enviromento"}
+							/>
+						</View>
 					</View>
 				</View>
 			)}
