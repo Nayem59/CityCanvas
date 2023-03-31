2bc0d8cda333114e828c940f8aa82d9105dd4b0c
Nayem <nayem.alam97@gmail.com>
2023-03-31 22:11:42 +0100

    merge conflict 3

--- CityCanvas/nav/HomeStack.js
+++ CityCanvas/nav/HomeStack.js
@@ -2,10 +2,7 @@ import React from "react";
 import AddArt from "../screens/AddArt";
 import Profile from "../screens/Profile";
 import Home from "../screens/Home";
-<<<<<<< HEAD
-=======
 import { useState } from "react";
->>>>>>> 9856ef49dff5b018d234c0024d02f0f6602e4e26
 
 import { Ionicons } from "@expo/vector-icons";
 
@@ -14,31 +11,47 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 
 const HomeStack = ({ uid }) => {
-<<<<<<< HEAD
+  const [renderComponent, setRenderComponent] = useState(false);
+
   return (
     <Tab.Navigator
       screenOptions={{ headerShown: false, tabBarActiveTintColor: "#C13584" }}
+      initialRouteName="Home"
     >
       <Tab.Screen
         name="Home"
-        component={Home}
         options={{
           tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="md-home" size={size} color={color} />
           ),
         }}
-      />
+      >
+        {(props) => (
+          <Home
+            {...props}
+            objectProp={{ renderComponent, setRenderComponent }}
+          />
+        )}
+      </Tab.Screen>
       <Tab.Screen
         name="AddArt"
-        component={AddArt}
+        // component={AddArt}
         options={{
           tabBarLabel: "Add Art",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="md-add-circle" size={size} color={color} />
           ),
         }}
-      />
+      >
+        {(props) => (
+          <AddArt
+            {...props}
+            setRenderComponent={setRenderComponent}
+            renderComponent={renderComponent}
+          />
+        )}
+      </Tab.Screen>
       <Tab.Screen
         name="Profile"
         // component={Profile}
@@ -53,63 +66,6 @@ const HomeStack = ({ uid }) => {
       </Tab.Screen>
     </Tab.Navigator>
   );
-=======
-	const [renderComponent, setRenderComponent] = useState(false);
-
-	return (
-		<Tab.Navigator
-			screenOptions={{ headerShown: false, tabBarActiveTintColor: "#C13584" }}
-			initialRouteName="Home"
-		>
-			<Tab.Screen
-				name="Home"
-				options={{
-					tabBarLabel: "Home",
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name="md-home" size={size} color={color} />
-					),
-				}}
-			>
-				{(props) => (
-					<Home
-						{...props}
-						objectProp={{ renderComponent, setRenderComponent }}
-					/>
-				)}
-			</Tab.Screen>
-			<Tab.Screen
-				name="AddArt"
-				// component={AddArt}
-				options={{
-					tabBarLabel: "Add Art",
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name="md-add-circle" size={size} color={color} />
-					),
-				}}
-			>
-				{(props) => (
-					<AddArt
-						{...props}
-						setRenderComponent={setRenderComponent}
-						renderComponent={renderComponent}
-					/>
-				)}
-			</Tab.Screen>
-			<Tab.Screen
-				name="Profile"
-				// component={Profile}
-				options={{
-					tabBarLabel: "Profile",
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name="md-person" size={size} color={color} />
-					),
-				}}
-			>
-				{(props) => <Profile {...props} uid={uid} />}
-			</Tab.Screen>
-		</Tab.Navigator>
-	);
->>>>>>> 9856ef49dff5b018d234c0024d02f0f6602e4e26
 };
 
 export default HomeStack;
