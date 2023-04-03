8b022882a6154d1c3dd014f9e98df71b553b80a9
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-04-03 09:48:57 +0100

    merge handled

--- CityCanvas/components/MapCard.js
+++ CityCanvas/components/MapCard.js
@@ -2,11 +2,11 @@ import { View, Text } from "react-native";
 import React from "react";
 
 const MapCard = () => {
-  return (
-    <View>
-      <Text>MapCard</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>MapCard</Text>
+		</View>
+	);
 };
 
 export default MapCard;
--- CityCanvas/screens/EditProfile.js
+++ CityCanvas/screens/EditProfile.js
@@ -8,67 +8,66 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db, auth } from "../firebaseConfig";
 
 const EditProfileSchema = Yup.object({
-  firstName: Yup.string()
-    .min(2, "Too short")
-    .max(20, "Too long")
-    .required("Required"),
-  lastName: Yup.string()
-    .min(2, "Too short")
-    .max(30, "Too long")
-    .required("Required"),
+	firstName: Yup.string()
+		.min(2, "Too short")
+		.max(20, "Too long")
+		.required("Required"),
+	lastName: Yup.string()
+		.min(2, "Too short")
+		.max(30, "Too long")
+		.required("Required"),
 });
 
 const EditProfile = ({ navigation, user, uid, setProfileChange }) => {
-  const editUserProfile = (firstName, lastName) => {
-    const docRef = doc(db, "users", uid);
-    updateDoc(docRef, { firstName, lastName }).then(() => {
-      setProfileChange(true);
-    });
-  };
+	const editUserProfile = (firstName, lastName) => {
+		const docRef = doc(db, "users", uid);
+		updateDoc(docRef, { firstName, lastName }).then(() => {
+			setProfileChange(true);
+		});
+	};
 
-  return (
-    <View className="bg-white w-100 h-100">
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-        }}
-        validationSchema={EditProfileSchema}
-        onSubmit={({ firstName, lastName }) => {
-          editUserProfile(firstName, lastName);
-        }}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-        }) => (
-          <View className="flex items-center justify-center w-10/12 mx-auto mb-10">
-            <Text className="mt-10 mb-10 text-3xl text-center">
-              <Text className="font-bold text-pink">Edit Detail</Text>
-            </Text>
-            <View className="w-full">
-              <TextInput
-                placeholder={user.firstName}
-                value={values.firstName}
-                onChangeText={handleChange("firstName")}
-                onBlur={handleBlur("firstName")}
-                autoCapitalize="none"
-                autoCorrect={false}
-                className="w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink"
-              />
-              {errors.firstName && touched.firstName && (
-                <View className="flex flex-row justify-start ml-2 ">
-                  <AntDesign name="exclamationcircleo" size={20} color="red" />
-                  <Text className="ml-2 text-red-600">{errors.firstName}</Text>
-                </View>
-              )}
-            </View>
+	return (
+		<View className="bg-white w-100 h-100">
+			<Formik
+				initialValues={{
+					firstName: "",
+					lastName: "",
+				}}
+				validationSchema={EditProfileSchema}
+				onSubmit={({ firstName, lastName }) => {
+					editUserProfile(firstName, lastName);
+				}}
+			>
+				{({
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					values,
+					errors,
+					touched,
+				}) => (
+					<View className="flex items-center justify-center w-10/12 mx-auto mb-10">
+						<Text className="mt-10 mb-10 text-3xl text-center">
+							<Text className="font-bold text-pink">Edit Detail</Text>
+						</Text>
+						<View className="w-full">
+							<TextInput
+								placeholder={user.firstName}
+								value={values.firstName}
+								onChangeText={handleChange("firstName")}
+								onBlur={handleBlur("firstName")}
+								autoCapitalize="none"
+								autoCorrect={false}
+								className="w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink"
+							/>
+							{errors.firstName && touched.firstName && (
+								<View className="flex flex-row justify-start ml-2 ">
+									<AntDesign name="exclamationcircleo" size={20} color="red" />
+									<Text className="ml-2 text-red-600">{errors.firstName}</Text>
+								</View>
+							)}
+						</View>
 
-<<<<<<< HEAD
 						<View className="w-full">
 							<TextInput
 								placeholder={user.lastName}
@@ -92,7 +91,7 @@ const EditProfile = ({ navigation, user, uid, setProfileChange }) => {
 							icon={"save"}
 						/>
 						<AppButton
-							handlePress={() => navigation.navigate("Profile")}
+							handlePress={() => navigation.goBack()}
 							title="Back to profile"
 							primary={false}
 							icon={"arrowleft"}
@@ -102,41 +101,6 @@ const EditProfile = ({ navigation, user, uid, setProfileChange }) => {
 			</Formik>
 		</View>
 	);
-=======
-            <View className="w-full">
-              <TextInput
-                placeholder={user.lastName}
-                value={values.lastName}
-                onChangeText={handleChange("lastName")}
-                onBlur={handleBlur("lastName")}
-                autoCapitalize="none"
-                className="w-full p-3 py-4 my-2 border rounded-lg border-stone-300 focus:border-pink"
-              />
-              {errors.lastName && touched.lastName && (
-                <View className="flex flex-row justify-start ml-2 ">
-                  <AntDesign name="exclamationcircleo" size={20} color="red" />
-                  <Text className="ml-2 text-red-600">{errors.lastName}</Text>
-                </View>
-              )}
-            </View>
-            <AppButton
-              handlePress={handleSubmit}
-              title="Save Changes"
-              primary={true}
-              icon={"save"}
-            />
-            <AppButton
-              handlePress={() => navigation.goBack()}
-              title="Back to profile"
-              primary={false}
-              icon={"arrowleft"}
-            />
-          </View>
-        )}
-      </Formik>
-    </View>
-  );
->>>>>>> main
 };
 
 export default EditProfile;
